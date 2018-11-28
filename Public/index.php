a73ccca5c6332ddbaffb8dd738dd107c32f46a52
bertin.dev <bertmoun@yahoo.fr>
2018-11-28 18:17:59 +0100

    update web page index.php with using the ternaire

---
+++ Public/index.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Supers-Pipo
+ * Date: 05/08/2018
+ * Time: 08h52
+ */
+
+
+session_start();
+
+//page de demarrage
+isset($_GET['id_page']) ? $page = $_GET['id_page'] : $page=1;
+  $_SESSION['id_page'] = $page;
+
+//mise en cache du oorps de toutes les pages du site
+  ob_start();
+
+  switch($page){
+
+      case 1:
+          require '../Pages/Home.php';
+          break;
+
+      case 2:
+          require '../pages/Portfolio.php';
+          break;
+
+      case 3:
+          require '../Pages/Competences.php';
+          break;
+
+      case 4:
+          require '../Pages/Culture.php';
+          break;
+
+      case 5:
+          require '../Pages/Contact.php';
+          break;
+
+      case 404:
+          require '../Pages/404.php';
+          break;
+
+      default:
+          {
+              header("HTTP/1.0 404 Not Found");
+              header('Location:index.php?id_page=404');
+          }
+
+  }
+
+$contenu = ob_get_clean();
+require '../Pages/Templates/default.php';
+
+
+
