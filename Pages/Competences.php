b97770549caef48c7e4f7f0bcbdfae6d655614d3
bertin.dev <bertmoun@yahoo.fr>
2018-11-28 18:47:08 +0100

    call the web page 'page_number.php' in my header

---
+++ Pages/Competences.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Supers-Pipo
+ * Date: 03/02/2018
+ * Time: 10h04
+ */
+require_once('page_number.php'); ?>
+
+
+
+<!-- Photos de Profil -->
+<section id="" class="">
+    <div class="container">
+        <div class="row">
+
+
+
+
+            <div class="col-lg-6">
+
+                <h1 align="center">Web</h1>
+                    <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                            <div class="col-lg-3"><p><strong>HTML/CSS</strong><br><span title="<?= date("Y")-2011 .' ans d\'expérience dans l\'utilisation du HTML/CSS';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2011 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/html5.png" alt="html5" title="HTML5 / CSS3"> </div>
+                            <div class="col-lg-3"><p><strong>PHP</strong><br><span title="<?= date("Y")-2014 .' ans d\'expérience dans l\'utilisation de PHP';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2014 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/php.png" alt="PHP" title="PHP"> </div>
+                            <div class="col-lg-3"><p><strong>JavaScript</strong><br><span title="<?= date("Y")-2017 .' an d\'expérience dans l\'utilisation du JavaScript';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2017 .' an d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/js.png" alt="JS" title="JavaScript"> </div>
+                            <div class="col-lg-3"><p><strong>SQL</strong><br><span title="<?= date("Y")-2015 .' ans d\'expérience dans l\'utilisation du SQL';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2015 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/sql.png" alt="sql" title="SQL"> </div>
+                    </div>
+
+                <h1 align="center">Framework</h1>
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                    <div class="col-lg-3"><p><strong>jQuery</strong><br><span title="<?= date("Y")-2017 .' an d\'expérience dans l\'utilisation de jQuery Pour JS';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2017 .' an d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/jQuery.png" alt="jQuery" title="jQuery"> </div>
+                    <div class="col-lg-3"><p><strong>Bootstrap</strong><br><span title="<?= date("Y")-2016 .' ans d\'expérience dans l\'utilisation de bootstrap de Twitter';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2016 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/bootstrap.png" alt="PHP" title="Bootstrap de Twitter"> </div>
+                    <div class="col-lg-3"><p><strong>Symfony</strong><br><span title="<?= date("Y")-2017 .' an d\'expérience dans l\'utilisation de Symfony Pour PHP';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2017 .' an d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/symfony.png" alt="Symfony" title="Symfony"> </div>
+                </div>
+
+                <h1 align="center">CMS</h1>
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                    <div class="col-lg-3"><p><strong>Wordpress</strong><br><span title="<?= date("Y")-2015 .' ans d\'expérience dans l\'utilisation de Wordpress';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2015 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/wordpress.png" alt="Wordpress" title="Wordpress"> </div>
+                    <div class="col-lg-3"><p><strong>Prestashop</strong><br><span title="<?= date("Y")-2016 .' ans d\'expérience dans l\'utilisation de Prestashop';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2016 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/prestashop.png" alt="Prestashop" title="Prestashop"> </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <div class="col-lg-6">
+
+                <h1 align="center">SGBD</h1>
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                    <div class="col-lg-3"><p><strong>MS Access</strong><br><span title="<?= date("Y")-2015 .' ans d\'expérience dans l\'utilisation de MS Access';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2015 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/ms access.png" alt="Ms Access" title="Ms Access"> </div>
+                    <div class="col-lg-3"><p><strong>MySQL</strong><br><span title="<?= date("Y")-2014 .' ans d\'expérience dans l\'utilisation de MySQL';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2014 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/mysql.png" alt="MySQL" title="MySQL"> </div>
+                    <div class="col-lg-3"><p><strong>SQL Server</strong><br><span title="<?= date("Y")-2015 .' ans d\'expérience dans l\'utilisation de SQL Server';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2015 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/sql serveur.png" alt="SQL SERVER" title="SQL SERVER"> </div>
+                </div>
+
+                <h1 align="center">Exécutable</h1>
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                    <div class="col-lg-3"><p><strong>"C"</strong><br><span title="<?= date("Y")-2010 .' ans d\'expérience dans l\'utilisation du Langage C ';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2010 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/C.png" alt="Langage C" title="Langage C"> </div>
+                    <div class="col-lg-3"><p><strong>"C#"</strong><br><span title="<?= date("Y")-2015 .' ans d\'expérience dans l\'utilisation du Langage C#';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2015 .' ans d\'expérience';?></span></p><img class="img-responsive" width="70" src="../Public/img/competences/csharp.png" alt="Langage C#" title="Langage C#"> </div>
+                    <div class="col-lg-3"><p><strong>JAVA SE</strong><br><span title="<?= date("Y")-2016 .' ans d\'expérience dans l\'utilisation du Langage Java';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2016 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/java se.png" alt="Langage Java" title="Langage Java"> </div>
+                </div>
+
+                <h1 align="center">OS</h1>
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                    <div class="col-lg-3"><p><strong>Windows</strong><br><span title="<?= date("Y")-2007 .' ans d\'expérience dans l\'utilisation de Windows';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2007 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/windows.png" alt="Windows" title="Windows"> </div>
+                    <div class="col-lg-3"><p><strong>Linux</strong><br><span title="<?= date("Y")-2012 .' ans d\'expérience dans l\'utilisation de Linux';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2012 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/kali linux.png" alt="Linux" title="Linux"> </div>
+                    <div class="col-lg-3"><p><strong>Android</strong><br><span title="<?= date("Y")-2013 .' ans d\'expérience dans l\'utilisation d\'Android';?>" style="color: #8d8f92; font-size: 11px;"><?= date("Y")-2013 .' ans d\'expérience';?></span></p><img class="img-responsive" src="../Public/img/competences/android.png" alt="Android" title="Android"> </div>
+                </div>
+
+            </div>
+
+
+
+
+
+
+            <h4 align="left">Technologies<small><em> Les outils que j'utilise</em></small></h4>
+            <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+                <div class="col-lg-2"><p><strong>IntelliJ IDEA</strong><br><span title="IntelliJ IDEA" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/intellij.png" alt="IntelliJ IDEA" title="IntelliJ IDEA"> </div>
+                <div class="col-lg-2"><p><strong>PHPSTORM 2017.1.1</strong><br><span title="PHPSTORM 2017.1.1" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/phpstorm.png" WIDTH="110" alt="phpstorm" title="PHPSTORM 2017.1.1"> </div>
+                <div class="col-lg-2"><p><strong>VISUAL STUDIO CODE</strong><br><span title="MS VISUAL STUDIO CODE" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/ms_vs_code.png" WIDTH="110" alt="ms_vs_code" title="MS VISUAL STUDIO CODE"> </div>
+                <div class="col-lg-2"><p><strong>VISUAL STUDIO 2012</strong><br><span title="MS VISUAL STUDIO PREMIUM 2012" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/ms_vs_studio.png" WIDTH="110" alt="ms_vs_studio" title="MS VISUAL STUDIO PREMIUM 2012"> </div>
+                <div class="col-lg-2"><p><strong>ECLIPSE LUMIA</strong><br><span title="ECLIPSE LUMIA" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/eclipse_lumia.png" WIDTH="110" alt="eclipse_lumia" title="ECLIPSE LUMIA"> </div>
+                <div class="col-lg-2"><p><strong>ANDROID STUDIO 2.0</strong><br><span title="ANDROID STUDIO 2.0" style="color: #8d8f92; font-size: 13px;">Editeur</span></p><img class="img-responsive" src="../Public/img/competences/android_studio.jpg" WIDTH="110" alt="Android Studio" title="ANDROID STUDIO 2.0"> </div>
+            </div>
+
+                <div class="col-lg-12 well" align="center" style="background: url('../Public/img/pattern15.png');">
+
+                <div class="col-lg-2"><p><strong>Google Chrome</strong><br><span title="Google Chrome" style="color: #8d8f92; font-size: 13px;">Navigateur</span></p><img class="img-responsive" src="../Public/img/competences/chrome.png"  alt="Google Chrome" title="Google Chrome"> </div>
+                <div class="col-lg-2"><p><strong>Firefox Developper</strong><br><span title="Mozilla Firefox Edition Developper" style="color: #8d8f92; font-size: 13px;">Navigateur</span></p><img class="img-responsive" WIDTH="110" src="../Public/img/competences/mozilla.jpg" alt="Mozilla Firefox Edition Developper" title="Mozilla Firefox Edition Developper"> </div>
+                <div class="col-lg-2"><p><strong>XAMPP SERVEUR</strong><br><span title="XAMPP" style="color: #8d8f92; font-size: 13px;">Serveur Locale</span></p><img class="img-responsive" src="../Public/img/competences/xampp.png" alt="XAMPP SERVEUR" title="XAMPP SERVEUR"> </div>
+                <div class="col-lg-2"><p><strong>Git</strong><br><span title="Git" style="color: #8d8f92; font-size: 13px;">Versioning</span></p><img class="img-responsive" src="../Public/img/competences/git.png" alt="Git" title="Git"> </div>
+                <div class="col-lg-2"><p><strong>Gulp</strong><br><span title="gulp" style="color: #8d8f92; font-size: 13px;">Automatisation</span></p><img class="img-responsive" src="../Public/img/competences/gulp.png" alt="gulp" title="gulp"> </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</section>
+
+
+
+
+
+
