c8d8a00356fd0ddec3644a4dfd0349f934fe4131
bertin.dev <bertmoun@yahoo.fr>
2018-12-26 11:51:00 +0100

    travail sur la page A Propos

--- Pages/Apropos.php
+++ Pages/Apropos.php
@@ -9,11 +9,108 @@
 <?php require_once('page_number.php'); ?>
 
 
-<section>
+<section id="" class="">
+<div class="container" style="padding: initial; margin: initial; width: 100%">
+    <div class="row" style="padding: initial; margin: initial; width: 100%">
 
-    <div class="parallax" style="padding: initial; margin-top: -40px; height: 100px;">
-        <p>
-        <h1 style="position: relative; top: 80px;">GOOGLE DEVELOPPER</h1></p>
+
+   <!-- <div class="apropos-cover" style="padding: initial; margin-top: -40px; height: 200px;">
+        <div class="col-lg-3">
+            <img src="img/Accueil/app.jpg" alt="" class="img-responsive img-circle">
+        </div>
+
+        <div class="col-lg-9">
+        </div>
+    </div>-->
+
+    <div class="col-lg-12 apropos-cover" style="border: 5px solid green; ">
+
     </div>
+        <div class="col-lg-12" style="background: white; margin-bottom: 20px;">
+            <div class="col-lg-3">
+                <img src="img/Accueil/app.jpg" alt="" style="float: right; margin-top: -70px; z-index: 10;border: 2px solid white;" class="img-circle" width="130" height="130">
+            </div>
+            <div class="col-lg-9" style="color: black">
+            <p style="font-variant: small-caps;">
+                <span style="font-size: 26px; font-weight: bold;">Bertin Mounok</span><br>
+                <em>Developpeur d'Applications Informatique</em><br>
+                <hr style="margin: initial; padding: initial; width: 100px;">
+                <i> A l'écoute d'opportunités</i>
+            </p>
+            </div>
+        </div>
 
+
+        <div class="col-lg-4" style="padding-left: initial;">
+
+          <div class="col-lg-12 apropos-cover" style="height: 100px;">
+                  <h4 style="position: absolute; left: 40%; top: 50%; color: #2487c7;font-variant: small-caps;">#Bertin Mounok <br> <small><em>Cameroun</em></small></h4>
+          </div>
+            <div class="col-lg-12" style="background: white;">
+                <div class="col-lg-5">
+                    <img src="img/Accueil/app.jpg" alt="" style="float: right; margin-top: -45px; z-index: 10;" class="img-responsive img-thumbnail">
+                </div>
+                <div class="col-lg-7" style="color: black">
+                <img src="img/Accueil/app.jpg" alt="" class="img-circle" width="40" height="40" style="float: left; margin-right: 2px;">
+                    <img src="img/Accueil/app.jpg" alt="" class="img-circle" width="40" height="40" style="float: left; margin-right: 2px;">
+                    <img src="img/Accueil/app.jpg" alt="" class="img-circle" width="40" height="40" style="float: left; margin-right: 2px;">
+                    <img src="img/Accueil/app.jpg" alt="" class="img-circle" width=40" height="40" >
+                </div>
+            </div>
+
+            <div class="col-lg-12">
+                <q>#bertin.dev</q> un Geek de l'informatique en général et la programmation en particulier
+                depuis l'âge de 18 ans. Au tout début j' avais commencé par être passionné des Jeux Vidéos
+                (Gamer), ensuite j'ai attaqué l'installation des logiciels, des Jeux PC, des systèmes d'exploitations,
+                le crackage, patching des logiciels, le surf illégal, les téléchargements légaux et illégaux et très vite
+                c'est devenu une passion que j'ai pu orienter vers le codage des logiciels d'entreprises depuis un bout de temps maintenant.
+            </div>
+        </div>
+
+        <div class="col-lg-5">
+            <div class="col-lg-12" style="background: white;">
+                <div class="col-lg-3"></div>
+                <div class="col-lg-9" style="color: black">
+                        <p>
+                            En formation chez Faculté Des Sciences
+                            <small>Précédents : CIs , Google Developper Group (GDG)</small>
+                        </p>
+                </div>
+            </div>
+            <div class="col-lg-12" style="Background: whitesmoke; color: black">
+                <div class="col-lg-5">
+                    <p><small>Poste: <em>Web Developper </em></small></p>
+                </div>
+                <div class="col-lg-7 right-sidebar" style="color: black">
+                    <p>
+                        <small>Yaoundé, Centre
+                        Cameroun</small>
+                    </p>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="col-lg-3">
+     <div class="col-lg-12" style="background: white;">
+         <p style="color: black">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+             Accusamus consequatur cum deserunt, distinctio dolore,
+             dolorum ea eius magnam maxime necessitatibus nulla porro quam quia,
+             recusandae saepe sapiente similique soluta veritatis.
+         </p>
+     </div>
+        </div>
+
+
+        <div class="col-lg-8">
+            <div class="col-lg-12" style="background: white; height: 200px; margin-top: 20px;">
+
+            </div>
+
+        </div>
+
+
+    </div>
+</div>
 </section>
